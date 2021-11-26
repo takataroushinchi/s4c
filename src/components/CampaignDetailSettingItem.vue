8108bfac27ed597bce399d72b92a3ec21143bcbf
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2021-11-26 11:30:24 +0900

    キャンペーン詳細、絞り込み 項目変更、サマリ情報変更

--- src/components/CampaignDetailSettingItem.vue
+++ src/components/CampaignDetailSettingItem.vue
@@ -3,8 +3,8 @@
   <ul class="c-InlineList__items u-FlexBox u-FlexBox--middle">
     <li class="c-InlineList__item"><span>公開状態：有効</span></li>
     <li class="c-InlineList__item"><span>期間：2021/10/10 - 2021/11/10</span><i>期間超過</i></li>
-    <li class="c-InlineList__item"><span>日予算：9,999</span></li>
-    <li class="c-InlineList__item"><span>利用額：5,000</span><i>予算超過</i></li>
+    <li class="c-InlineList__item"><span>利用状況（当日利用額 / 日予算）：5,000 / 9,999</span><i>予算超過</i></li>
+    <li class="c-InlineList__item"><span>全期間利用額：9,999</span></li>
   </ul><!-- /c-InlineList__items  -->
 </div><!-- /c-InlineList  -->
 </template>
--- src/components/CampaignDetailTool.vue
+++ src/components/CampaignDetailTool.vue
@@ -22,26 +22,6 @@
 
     <div class="u-FlexBox__spacer"></div>
 
-    <div class="c-Radio__group">
-      <div class="c-Radio__groupLavel">
-        <span>入札状況</span>
-      </div>
-      <div class="c-Radio c-Radio--horizontal">
-        <input class="c-Radio__radio" type="radio" id="dummyID-01-01" name="dummyID-01" checked>
-        <label class="c-Radio__label" for="dummyID-01-01"><span>有効</span></label>
-      </div><!--/c-Radio-->
-      <div class="c-Radio c-Radio--horizontal">
-        <input class="c-Radio__radio" type="radio" id="dummyID-01-02" name="dummyID-01">
-        <label class="c-Radio__label" for="dummyID-01-02"><span>一時停止</span></label>
-      </div><!--/c-Radio-->
-      <div class="c-Radio c-Radio--horizontal">
-        <input class="c-Radio__radio" type="radio" id="dummyID-01-03" name="dummyID-01">
-        <label class="c-Radio__label" for="dummyID-01-03"><span>アーカイブ</span></label>
-      </div><!--/c-Radio-->
-    </div><!-- /c-Radio__group -->
-
-    <div class="u-FlexBox__spacer"></div>
-
     <div class="c-Tool__actions u-FlexBox u-FlexBox--middle">
       <button type="button" class="c-Button">
         <span>絞り込み</span>

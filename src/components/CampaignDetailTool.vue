0b223456f8323ca478d0c3c07132122c0c828cc3
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2021-11-26 11:47:31 +0900

    キャンペーン詳細絞り込み、項目(ラベル)変更

--- src/components/CampaignDetailTool.vue
+++ src/components/CampaignDetailTool.vue
@@ -22,6 +22,26 @@
 
     <div class="u-FlexBox__spacer"></div>
 
+    <div class="c-Radio__group">
+      <div class="c-Radio__groupLavel">
+        <span>入札状況</span>
+      </div>
+      <div class="c-Radio c-Radio--horizontal">
+        <input class="c-Radio__radio" type="radio" id="dummyID-01-01" name="dummyID-01" checked>
+        <label class="c-Radio__label" for="dummyID-01-01"><span>全て</span></label>
+      </div><!--/c-Radio-->
+      <div class="c-Radio c-Radio--horizontal">
+        <input class="c-Radio__radio" type="radio" id="dummyID-01-02" name="dummyID-01">
+        <label class="c-Radio__label" for="dummyID-01-02"><span>入札済み</span></label>
+      </div><!--/c-Radio-->
+      <div class="c-Radio c-Radio--horizontal">
+        <input class="c-Radio__radio" type="radio" id="dummyID-01-03" name="dummyID-01">
+        <label class="c-Radio__label" for="dummyID-01-03"><span>未入札</span></label>
+      </div><!--/c-Radio-->
+    </div><!-- /c-Radio__group -->
+
+    <div class="u-FlexBox__spacer"></div>
+
     <div class="c-Tool__actions u-FlexBox u-FlexBox--middle">
       <button type="button" class="c-Button">
         <span>絞り込み</span>

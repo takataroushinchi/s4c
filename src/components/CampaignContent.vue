80ac5b82b1a478277a5593ce101b929e28ce21d5
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2022-02-07 15:20:13 +0900

    キャンペーン必須入力項目変更

--- src/components/CampaignContent.vue
+++ src/components/CampaignContent.vue
@@ -11,8 +11,13 @@
       <div class="c-Content__unit">
         <h3 class="c-Content__unitTitle">キャンペーン名称</h3>
         <div class="c-Input__group">
-          <input class="c-Input" type="text" name="campaign_name" value="無題のキャンペーン001" spellcheck="false" placeholder="">
-          <div class="c-Input__feedback">エラーです</div>
+          <div class="c-Input__label">
+            <label>
+              名称<i class="c-Input__required">※必須</i>
+            </label>
+            <input class="c-Input" type="text" name="campaign_name" value="無題のキャンペーン001" spellcheck="false" placeholder="">
+            <div class="c-Input__feedback">エラーです</div>
+          </div><!-- /c-Input__label -->
         </div><!-- /c-Input__group -->
       </div><!-- /c-Content__unit -->
 
@@ -50,7 +55,7 @@
           </div><!-- /c-Input__label -->
           <div class="c-Input__label">
             <label>
-              開始時間
+              開始時間<i class="c-Input__required">※必須</i>
             </label>
             <!-- <select class="c-Input" id="startAtTime">
               <option value="0">00:00</option><option value="1">01:00</option><option value="2">02:00</option><option value="3">03:00</option><option value="4">04:00</option><option value="5">05:00</option><option value="6">06:00</option><option value="7">07:00</option><option value="8">08:00</option><option value="9">09:00</option><option value="10">10:00</option><option value="11">11:00</option><option value="12">12:00</option><option value="13">13:00</option><option value="14">14:00</option><option value="15">15:00</option><option value="16">16:00</option><option value="17">17:00</option><option value="18">18:00</option><option value="19">19:00</option><option value="20">20:00</option><option value="21">21:00</option><option value="22">22:00</option><option value="23">23:00</option>
@@ -74,7 +79,7 @@
         <div class="u-FlexBox u-FlexBox--top">
           <div class="c-Input__label">
             <label>
-              終了日
+              終了日<i class="c-Input__required">※必須</i>
             </label>
             <date-picker
              v-model="date2"
@@ -86,7 +91,7 @@
           </div><!-- /c-Input__label -->
           <div class="c-Input__label">
             <label>
-              終了時間
+              終了時間<i class="c-Input__required">※必須</i>
             </label>
             <!-- <select class="c-Input" id="endAtTime" disabled>
               <option value="0">-</option> <option value="{{n}}">01:59</option><option value="{{n}}">02:59</option><option value="{{n}}">03:59</option><option value="{{n}}">04:59</option><option value="{{n}}">05:59</option><option value="{{n}}">06:59</option><option value="{{n}}">07:59</option><option value="{{n}}">08:59</option><option value="{{n}}">09:59</option><option value="{{n}}">10:59</option><option value="{{n}}">11:59</option><option value="{{n}}">12:59</option><option value="{{n}}">13:59</option><option value="{{n}}">14:59</option><option value="{{n}}">15:59</option><option value="{{n}}">16:59</option><option value="{{n}}">17:59</option><option value="{{n}}">18:59</option><option value="{{n}}">19:59</option><option value="{{n}}">20:59</option><option value="{{n}}">21:59</option><option value="{{n}}">22:59</option><option value="{{n}}">23:59</option>
@@ -114,7 +119,7 @@
           <div class="c-Input__group">
             <div class="c-Input__label">
               <label>
-                日予算
+                日予算<i class="c-Input__required">※必須</i>
               </label>
               <input class="c-Input" type="number" name="budget" value="0" placeholder="">
               <div class="c-Input__feedback">エラーです</div>
@@ -123,7 +128,7 @@
           <div class="c-Input__group">
             <div class="c-Input__label">
               <label>
-                ベース入札金額
+                ベース入札金額<i class="c-Input__required">※必須</i>
               </label>
               <input class="c-Input" type="number" name="base_amount" value="50" placeholder="">
               <div class="c-Input__feedback">エラーです</div>

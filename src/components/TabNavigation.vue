47b3971afdfbe6733a2deebbd0157db43fe0cc49
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2022-03-05 08:41:07 +0900

    タブUI、tabindex設定

--- src/components/TabNavigation.vue
+++ src/components/TabNavigation.vue
@@ -2,10 +2,10 @@
   <div class="c-Tab">
     <ul class="c-Tab__items u-FlexBox u-FlexBox--middle">
       <li class="c-Tab__item">
-        <router-link :to="{name: 'UserHome'}" exact-active-class="is-selected" class="c-Button _tab">有効と一時停止</router-link>
+        <router-link :to="{name: 'UserHome'}" exact-active-class="is-selected" :tabindex="$route.name === 'UserHome'? -1 : 0" class="c-Button _tab">有効と一時停止</router-link>
       </li><!-- /c-Tab__item -->
       <li class="c-Tab__item">
-        <router-link :to="{name: 'UserHomeArchive'}" exact-active-class="is-selected" class="c-Button _tab">アーカイブのみ</router-link>
+        <router-link :to="{name: 'UserHomeArchive'}" exact-active-class="is-selected" :tabindex="$route.name === 'UserHomeArchive'? -1 : 0" class="c-Button _tab">アーカイブのみ</router-link>
       </li><!-- /c-Tab__item -->
     </ul><!-- /c-Tab__items -->
   </div><!-- /c-Tab -->

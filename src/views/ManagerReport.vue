1351a527e9ec052b00df82adfe9e63f03d6f8455
Takahiro Shinchi <takahiro.shinchi@supership.jp>
2022-06-22 10:47:35 +0900

    Edit Props use script setup

--- src/views/ManagerReport.vue
+++ src/views/ManagerReport.vue
@@ -7,7 +7,7 @@
   <ReportList :list="listRef"/>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter, useRoute, onBeforeRouteLeave, onBeforeRouteUpdate } from 'vue-router';
@@ -19,86 +19,68 @@ import ReportTool from '@/components/ReportTool.vue';
 import ReportListTitle from '@/components/ReportListTitle.vue';
 import ReportList from '@/components/ReportList.vue';
 
-export default {
-  name: 'ManagerReport',
-  components: {
-    ManagerHeader,
-    CommonTitle,
-    Chart,
-    ReportTool,
-    ReportListTitle,
-    ReportList,
-  },
-  props: {
-    dimension: {
-      type: String,
-      default: 'daily' // :dimension [ daily, monthly, account ]
-    },
-  },
-  setup() {
-    const store = useStore();
-    const router = useRouter();
+// const props = defineProps({
+//   dimension: {
+//     type: String,
+//     default: 'daily' // :dimension [ daily, monthly, campaign ]
+//   },
+// })
 
-    // ECサイト権限以外はルートにリダイレクト
-    if(store.state.login_user?.role !== 2){
-      router.push({name: 'Root'})
-    }
+const store = useStore();
+const router = useRouter();
 
-    const route = useRoute();
-    const { dimension } = route.params
-
-    let listRef = ref([])
-    let dimensionRef = ref('')
-
-    // /report?filter=account
-    const query_filter = route.query.filter? route.query.filter : ''
-
-    const data = [[
-      { id: 1111, unit: '2021/10/10' },
-      { id: 2222, unit: '2021/10/09' },
-      { id: 3333, unit: '2021/10/08' },
-      { id: 4444, unit: '2021/10/07' },
-      { id: 5555, unit: '2021/10/06' },
-    ],
-    [
-      { id: 1111, unit: '2021/10' },
-      { id: 2222, unit: '2021/09' },
-      { id: 3333, unit: '2021/08' },
-      { id: 4444, unit: '2021/07' },
-    ],
-    [
-      { id: 1111, unit: 'アカウント名称001' },
-      { id: 2222, unit: '壱弐参肆伍陸漆捌玖拾002' },
-      { id: 3333, unit: '壱弐参肆伍陸漆捌玖拾壱弐参肆伍陸漆捌玖拾称003' },
-      { id: 4444, unit: '壱弐参肆伍陸漆捌玖拾004' },
-    ]]
-
-
-    // :dimension [ daily, monthly, account ]
-    // /Report の場合は、undefind なので、dailyを設定
-    dimensionRef.value = dimension? dimension : "daily";
-    // listRef.value = (path === '/report/account')? data[2] : (path === '/report/monthly')? data[1] : data[0];
-    listRef.value = (dimension === 'account')? data[2] : (dimension === 'monthly')? data[1] : data[0];
-
-    onBeforeRouteLeave((to) => {
-      const p = to.path.split("/").filter(e => Boolean(e));
-      const d = p[p.length - 1];
-      dimensionRef.value = d === 'report'? 'daily' : d;
-      listRef.value = (d === 'account')? data[2] : (d === 'monthly')? data[1] : data[0];
-    })
-
-    onBeforeRouteUpdate((to) => {
-      const p = to.path.split("/").filter(e => Boolean(e));
-      const d = p[p.length - 1];
-      dimensionRef.value = d === 'report'? 'daily' : d;
-      listRef.value = (d === 'account')? data[2] : (d === 'monthly')? data[1] : data[0];
-    })
-
-    return {
-      listRef,
-      dimensionRef,
-      query_filter,
-    }
-  }
+// ECサイト権限以外はルートにリダイレクト
+if(store.state.login_user?.role !== 2){
+  router.push({name: 'Root'})
 }
+
+const route = useRoute();
+const { dimension } = route.params
+
+let listRef = ref([])
+let dimensionRef = ref('')
+
+// /report?filter=account
+const query_filter = route.query.filter? route.query.filter : ''
+
+const data = [[
+  { id: 1111, unit: '2021/10/10' },
+  { id: 2222, unit: '2021/10/09' },
+  { id: 3333, unit: '2021/10/08' },
+  { id: 4444, unit: '2021/10/07' },
+  { id: 5555, unit: '2021/10/06' },
+],
+[
+  { id: 1111, unit: '2021/10' },
+  { id: 2222, unit: '2021/09' },
+  { id: 3333, unit: '2021/08' },
+  { id: 4444, unit: '2021/07' },
+],
+[
+  { id: 1111, unit: 'アカウント名称001' },
+  { id: 2222, unit: '壱弐参肆伍陸漆捌玖拾002' },
+  { id: 3333, unit: '壱弐参肆伍陸漆捌玖拾壱弐参肆伍陸漆捌玖拾称003' },
+  { id: 4444, unit: '壱弐参肆伍陸漆捌玖拾004' },
+]]
+
+
+// :dimension [ daily, monthly, account ]
+// /Report の場合は、undefind なので、dailyを設定
+dimensionRef.value = dimension? dimension : "daily";
+// listRef.value = (path === '/report/account')? data[2] : (path === '/report/monthly')? data[1] : data[0];
+listRef.value = (dimension === 'account')? data[2] : (dimension === 'monthly')? data[1] : data[0];
+
+onBeforeRouteLeave((to) => {
+  const p = to.path.split("/").filter(e => Boolean(e));
+  const d = p[p.length - 1];
+  dimensionRef.value = d === 'report'? 'daily' : d;
+  listRef.value = (d === 'account')? data[2] : (d === 'monthly')? data[1] : data[0];
+})
+
+onBeforeRouteUpdate((to) => {
+  const p = to.path.split("/").filter(e => Boolean(e));
+  const d = p[p.length - 1];
+  dimensionRef.value = d === 'report'? 'daily' : d;
+  listRef.value = (d === 'account')? data[2] : (d === 'monthly')? data[1] : data[0];
+})
 </script>
--- src/views/Report.vue
+++ src/views/Report.vue
@@ -7,7 +7,7 @@
   <ReportList :list="listRef"/>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue'
 import { useRoute, onBeforeRouteLeave, onBeforeRouteUpdate } from 'vue-router';
 // @ is an alias to /src
@@ -18,78 +18,60 @@ import ReportTool from '@/components/ReportTool.vue';
 import ReportListTitle from '@/components/ReportListTitle.vue';
 import ReportList from '@/components/ReportList.vue';
 
-export default {
-  name: 'Report',
-  components: {
-    Header,
-    CommonTitle,
-    Chart,
-    ReportTool,
-    ReportListTitle,
-    ReportList,
-  },
-  props: {
-    dimension: {
-      type: String,
-      default: 'daily' // :dimension [ daily, monthly, campaign ]
-    },
-  },
-  setup() {
-    let listRef = ref([])
-    let dimensionRef = ref('')
-    const route = useRoute()
-    const { dimension } = route.params
-    // const path = route.path;
+// const props = defineProps({
+//   dimension: {
+//     type: String,
+//     default: 'daily' // :dimension [ daily, monthly, campaign ]
+//   },
+// })
 
-    // /report?filter=cpn
-    const query_filter = route.query.filter? route.query.filter : ''
+let listRef = ref([])
+let dimensionRef = ref('')
+const route = useRoute()
+const { dimension } = route.params
+// const path = route.path;
 
-    const data = [[
-      { id: 1111, unit: '2021/10/10' },
-      { id: 2222, unit: '2021/10/09' },
-      { id: 3333, unit: '2021/10/08' },
-      { id: 4444, unit: '2021/10/07' },
-      { id: 5555, unit: '2021/10/06' },
-    ],
-    [
-      { id: 1111, unit: '2021/10' },
-      { id: 2222, unit: '2021/09' },
-      { id: 3333, unit: '2021/08' },
-      { id: 4444, unit: '2021/07' },
-    ],
-    [
-      { id: 1111, unit: 'キャンペーン名称001' },
-      { id: 2222, unit: '壱弐参肆伍陸漆捌玖拾002' },
-      { id: 3333, unit: '壱弐参肆伍陸漆捌玖拾壱弐参肆伍陸漆捌玖拾称003' },
-      { id: 4444, unit: '壱弐参肆伍陸漆捌玖拾004' },
-    ]]
+// /report?filter=cpn
+const query_filter = route.query.filter? route.query.filter : ''
+
+const data = [[
+  { id: 1111, unit: '2021/10/10' },
+  { id: 2222, unit: '2021/10/09' },
+  { id: 3333, unit: '2021/10/08' },
+  { id: 4444, unit: '2021/10/07' },
+  { id: 5555, unit: '2021/10/06' },
+],
+[
+  { id: 1111, unit: '2021/10' },
+  { id: 2222, unit: '2021/09' },
+  { id: 3333, unit: '2021/08' },
+  { id: 4444, unit: '2021/07' },
+],
+[
+  { id: 1111, unit: 'キャンペーン名称001' },
+  { id: 2222, unit: '壱弐参肆伍陸漆捌玖拾002' },
+  { id: 3333, unit: '壱弐参肆伍陸漆捌玖拾壱弐参肆伍陸漆捌玖拾称003' },
+  { id: 4444, unit: '壱弐参肆伍陸漆捌玖拾004' },
+]]
 
 
-    // :dimension [ daily, monthly, campaign ]
-    // /Report の場合は、undefind なので、dailyを設定
-    dimensionRef.value = dimension? dimension : "daily";
-    // listRef.value = (path === '/report/campaign')? data[2] : (path === '/report/monthly')? data[1] : data[0];
-    listRef.value = (dimension === 'campaign')? data[2] : (dimension === 'monthly')? data[1] : data[0];
+// :dimension [ daily, monthly, campaign ]
+// /Report の場合は、undefind なので、dailyを設定
+dimensionRef.value = dimension? dimension : "daily";
+// listRef.value = (path === '/report/campaign')? data[2] : (path === '/report/monthly')? data[1] : data[0];
+listRef.value = (dimension === 'campaign')? data[2] : (dimension === 'monthly')? data[1] : data[0];
 
-    onBeforeRouteLeave((to) => {
-      const p = to.path.split("/").filter(e => Boolean(e));
-      const d = p[p.length - 1];
-      dimensionRef.value = d === 'report'? 'daily' : d;
-      listRef.value = (d === 'campaign')? data[2] : (d === 'monthly')? data[1] : data[0];
-    })
+onBeforeRouteLeave((to) => {
+  const p = to.path.split("/").filter(e => Boolean(e));
+  const d = p[p.length - 1];
+  dimensionRef.value = d === 'report'? 'daily' : d;
+  listRef.value = (d === 'campaign')? data[2] : (d === 'monthly')? data[1] : data[0];
+})
 
-    onBeforeRouteUpdate((to) => {
-      const p = to.path.split("/").filter(e => Boolean(e));
-      const d = p[p.length - 1];
-      dimensionRef.value = d === 'report'? 'daily' : d;
-      listRef.value = (d === 'campaign')? data[2] : (d === 'monthly')? data[1] : data[0];
-    })
-
-    return {
-      listRef,
-      dimensionRef,
-      query_filter,
-    }
-  }
-}
+onBeforeRouteUpdate((to) => {
+  const p = to.path.split("/").filter(e => Boolean(e));
+  const d = p[p.length - 1];
+  dimensionRef.value = d === 'report'? 'daily' : d;
+  listRef.value = (d === 'campaign')? data[2] : (d === 'monthly')? data[1] : data[0];
+})
 </script>
